3640d4606f20989829b0dba282c164761f8c4672
Lucas <lucasrealg12@gmail.com>
2024-03-24 11:10:30 -0300

    creating and configuring the app_create_cars

--- Cars_Registration/urls.py
+++ Cars_Registration/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from app_create_cars import views
 
 urlpatterns = [
-    
+    #rote, view adm and reference name
+    #cars.com/
+    path('', views.home,  name='home'), 
 ]
