9d80b5319fabdd27337556db177a941f9fa26fa2
Lucas <lucasrealg12@gmail.com>
2024-04-03 19:50:45 -0300

    all CRUD view methods interacting with http pages

--- app_create_cars/forms.py
+++ app_create_cars/forms.py
@@ -4,4 +4,8 @@ from .models import Car
 class  CarForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = [ 'id', 'name','year']
+        fields = [ 'car_id', 'car_name','car_year']
+        widgets = {
+            'car_name': forms.TextInput(attrs={'size':20} ),
+            'car_year': forms.Select(choices=((y, y) for y in range(2024, 1950, -1))),
+        }
